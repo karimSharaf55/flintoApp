e61c350d41b2107a71dc5531a47b33e3f6ab8295
karimSharaf55 <devkarim55@gmail.com>
2022-05-01 05:40:12 +0200

    Updates

---
+++ precache-manifest.fcb63a0839892f30391abcbf0dac6ab6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "10f9cf0316cf044ac1bb",
+    "url": "/flintoApp/static/css/main.6ee1d491.chunk.css"
+  },
+  {
+    "revision": "10f9cf0316cf044ac1bb",
+    "url": "/flintoApp/static/js/main.10f9cf03.chunk.js"
+  },
+  {
+    "revision": "6ff0d24605aac8490cf0",
+    "url": "/flintoApp/static/css/1.dfe29693.chunk.css"
+  },
+  {
+    "revision": "6ff0d24605aac8490cf0",
+    "url": "/flintoApp/static/js/1.6ff0d246.chunk.js"
+  },
+  {
+    "revision": "044146b4e4a56a175639",
+    "url": "/flintoApp/static/js/runtime~main.044146b4.js"
+  },
+  {
+    "revision": "b98d4390b5b3840d60d3b0a60d3f9ebe",
+    "url": "/flintoApp/static/media/category.b98d4390.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flintoApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "242f1ac3e93b90fda7e286f173f008c3",
+    "url": "/flintoApp/static/media/slide3.242f1ac3.jpg"
+  },
+  {
+    "revision": "47a7deff78f7969fad73c447be2838cf",
+    "url": "/flintoApp/static/media/login-bg.47a7deff.jpg"
+  },
+  {
+    "revision": "67fd76d3b325baacf3d915244a2ff413",
+    "url": "/flintoApp/static/media/coming.67fd76d3.jpg"
+  },
+  {
+    "revision": "6622dc23db51f1623aadabbc584709db",
+    "url": "/flintoApp/static/media/woman.6622dc23.jpg"
+  },
+  {
+    "revision": "700b9b3a2e8c3a292e75b4b40c44851a",
+    "url": "/flintoApp/static/media/item.700b9b3a.jpg"
+  },
+  {
+    "revision": "1a667b5aec5ea8bebccd1ec809617f35",
+    "url": "/flintoApp/static/media/Deal.1a667b5a.jpg"
+  },
+  {
+    "revision": "39c51097134fbab581ec7d2e0da62e5c",
+    "url": "/flintoApp/static/media/Accessories.39c51097.jpg"
+  },
+  {
+    "revision": "27348b7ecfeef5a8c5895f50aad985ef",
+    "url": "/flintoApp/static/media/man.27348b7e.jpg"
+  },
+  {
+    "revision": "8d069c52b89c37bdbd954b83c8093940",
+    "url": "/flintoApp/static/media/shoes.8d069c52.jpg"
+  },
+  {
+    "revision": "71b2ea4791c1ec7a8ad10c06badf1b3b",
+    "url": "/flintoApp/static/media/img-9.71b2ea47.jpeg"
+  },
+  {
+    "revision": "afcb2824c14e71625cb4995834befbbc",
+    "url": "/flintoApp/static/media/img-10.afcb2824.jpeg"
+  },
+  {
+    "revision": "a735b84756816fa5e6e231c472de9001",
+    "url": "/flintoApp/static/media/what.a735b847.jpeg"
+  },
+  {
+    "revision": "d7648fa001ab19fb2324bf573b466e38",
+    "url": "/flintoApp/static/media/what1.d7648fa0.jpeg"
+  },
+  {
+    "revision": "3bcbc7a5918446b1eb55e6aef58fe7f1",
+    "url": "/flintoApp/static/media/SliderPrice.3bcbc7a5.jpg"
+  },
+  {
+    "revision": "ed33b560fce0473c9bc2d9ce019c31e1",
+    "url": "/flintoApp/index.html"
+  }
+];
